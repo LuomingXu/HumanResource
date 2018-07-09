39e0ffcb44fe8d1eb025c2958127f836f2e8dc04
zhoujun1 <hayashifuu@qq.com>
2018-07-09 09:41:12 +0800

    init

---
+++ src/main/java/com/qst/human_resources/HumanResourcesApplication.java
@@ -0,0 +1,12 @@
+package com.qst.human_resources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourcesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumanResourcesApplication.class, args);
+    }
+}

256605b73b2926247419dcbf0acbc11750c071db
LuoMing Xu <24503603+LuomingXu@users.noreply.github.com>
2018-07-12 09:24:05 +0800

    download complete

--- src/main/java/com/qst/human_resources/controller/DownloadController.java
+++ src/main/java/com/qst/human_resources/controller/DownloadController.java
@@ -23,8 +23,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.io.File;
-import java.io.FileOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -45,7 +46,7 @@ public class DownloadController
 
     @ResponseBody
     @RequestMapping("/downloadExcel")
-    public String downloadExcelReport()
+    public String downloadExcelReport(HttpServletRequest request, HttpServletResponse response)
     {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         UserReport userReport;
@@ -53,9 +54,14 @@ public class DownloadController
         List<UserDTO> userLists;
         String temp;
 
+        String filePath = String.format(
+                "%s\\src\\main\\resources\\templates\\UserReport.xlsx",
+                System.getProperty("user.dir"));
+
         //获取所有用户
         userLists = userService.getAllUsers();
 
+        //获取数据, 创建文件
         try
         {
             for (UserDTO userDTO : userLists)
@@ -104,11 +110,10 @@ public class DownloadController
             System.err.println("write in");
 
             //创建xlsx文件
-            String filePath=String.format("%s\\src\\main\\resources\\templates\\UserReport.xlsx",
-                    System.getProperty("user.dir"));
+
             try
             {
-                File file=new File(filePath);
+                File file = new File(filePath);
 
                 if (file.exists())
                 {
@@ -141,6 +146,55 @@ public class DownloadController
         catch (Exception e)
         {
             e.printStackTrace();
+        }
+
+        //相应前台
+        File file = new File(filePath);
+        // 设置强制下载不打开
+        response.setContentType("application/force-download");
+        // 设置文件名
+        response.addHeader(
+                "Content-Disposition",
+                "attachment;fileName=" + file.getName());
+
+        byte[] buffer = new byte[1024];
+        FileInputStream fis = null;
+        BufferedInputStream bis = null;
+
+        try
+        {
+            fis = new FileInputStream(file);
+            bis = new BufferedInputStream(fis);
+            OutputStream os = response.getOutputStream();
+            int i = bis.read(buffer);
+            while (i != -1)
+            {
+                os.write(buffer, 0, i);
+                i = bis.read(buffer);
+            }
+            System.err.println("success");
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                if (bis != null)
+                {
+                    bis.close();
+                }
+                if (fis != null)
+                {
+                    fis.close();
+                }
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
         }
 
         return null;
